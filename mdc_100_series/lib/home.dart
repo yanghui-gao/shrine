1f8142977c0fdf64df70b6bfd78a0dde3360ef14
高炀辉 <insistgyh@foxmail.com>
2021-05-31 18:33:30 +0800

    添加导航栏以及标题

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -23,6 +23,9 @@ class HomePage extends StatelessWidget {
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        title: Text('SHRINE'),
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
