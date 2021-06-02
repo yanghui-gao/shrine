d39ec8f2ab0d42ecff67f8f6eef812ca6cc4fc99
高炀辉 <insistgyh@foxmail.com>
2021-06-02 11:24:23 +0800

    创建主题颜色

---
+++ mdc_100_series/lib/Colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const KshrinePink50 = const Color(0xFFFEEAE6);
+const KshrinePink100 = const Color(0xFFFEDBD0);
+const KshrinePink300 = const Color(0xFFFBB8AC);
+const KshrinePink400 = const Color(0xFFEAA4A4);
+
+const KshrineBrown900 = const Color(0xFF442B2D);
+
+const KshrineErrorRed = const Color(0xFFC5032B);
+
+const KshrineSurfaceWhite = const Color(0xFFFFFBFA);
+const KshrineBackgroundWhite = Colors.white;
